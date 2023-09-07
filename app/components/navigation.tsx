db1ea1ecb571263d64b2f16fa6fb527fb8634812
ellukasion <lukasion@gmail.com>
2023-09-07 17:34:26 +0200

    Correct Textfields

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -9,17 +9,16 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import {blueGrey, grey} from '@mui/material/colors';
-import { TextField } from "@mui/material";
-import { styled } from '@mui/system';
+import {TextField} from "@mui/material";
+import {styled} from '@mui/system';
 
 const pages = ['O aplikacji', 'Załóż profil obiektu', 'Zarejestruj się', 'Zaloguj się'];
 const coloredPages = ['Zarejestruj się', 'Zaloguj się']
+
 // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Navigation() {
@@ -47,21 +46,37 @@ function Navigation() {
 
     const WhiteBorderTextField = styled(TextField)`
       & label.Mui-focused,
-      & label{
-        color: white;
+      & label {
+        color: black;
+        background: white;
+        padding: 0px 10px;
+        border-radius: 5px;
       }
-      
+
       & input {
         background-color: white;
-        border-radius: 5px;
+        border-width: 3px;
+        border-color: #1976d2;
+        border-radius: 0;
+      }
+
+      & fieldset {
+        border-width: 0;
+        border-radius: 0;
+        border: 0 !important;
       }
     `;
 
     return (
-        <AppBar position="static" sx={{ minHeight: 300}} elevation={0}>
-            <Container maxWidth="xl">
+        <AppBar position="static" sx={{minHeight: 300}} elevation={0}>
+            <Container maxWidth="lg">
                 <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <AdbIcon sx={{
+                        display: {
+                            xs: 'none',
+                            md: 'flex'
+                        }, mr: 1
+                    }}/>
 
                     <Typography
                         variant="h6"
@@ -70,7 +85,7 @@ function Navigation() {
                         href="/"
                         sx={{
                             mr: 2,
-                            display: { xs: 'none', md: 'flex' },
+                            display: {xs: 'none', md: 'flex'},
                             fontFamily: 'monospace',
                             fontWeight: 700,
                             letterSpacing: '.3rem',
@@ -78,10 +93,10 @@ function Navigation() {
                             textDecoration: 'none',
                         }}
                     >
-                        Signiter.pl
+                        Signifer.pl
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
                         <IconButton
                             size="large"
                             aria-label="account of current user"
@@ -90,7 +105,7 @@ function Navigation() {
                             onClick={handleOpenNavMenu}
                             color="inherit"
                         >
-                            <MenuIcon />
+                            <MenuIcon/>
                         </IconButton>
                         <Menu
                             id="menu-appbar"
@@ -107,7 +122,7 @@ function Navigation() {
                             open={Boolean(anchorElNav)}
                             onClose={handleCloseNavMenu}
                             sx={{
-                                display: { xs: 'block', md: 'none' },
+                                display: {xs: 'block', md: 'none'},
                             }}
                         >
                             {pages.map((page) => (
@@ -118,7 +133,12 @@ function Navigation() {
                         </Menu>
                     </Box>
 
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <AdbIcon sx={{
+                        display: {
+                            xs: 'flex',
+                            md: 'none'
+                        }, mr: 1
+                    }}/>
 
                     <Typography
                         variant="h5"
@@ -127,7 +147,10 @@ function Navigation() {
                         href="/"
                         sx={{
                             mr: 2,
-                            display: { xs: 'flex', md: 'none' },
+                            display: {
+                                xs: 'flex',
+                                md: 'none'
+                            },
                             flexGrow: 1,
                             fontFamily: 'monospace',
                             fontWeight: 700,
@@ -139,7 +162,15 @@ function Navigation() {
                         LOGO
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, gap: 2, justifyContent: 'flex-end' }}>
+                    <Box sx={{
+                        flexGrow: 1,
+                        display: {
+                            xs: 'none',
+                            md: 'flex'
+                        },
+                        gap: 2,
+                        justifyContent: 'flex-end'
+                    }}>
                         {pages.map((page) => (
                             <Button
                                 key={page}
@@ -162,49 +193,40 @@ function Navigation() {
                             </Button>
                         ))}
                     </Box>
-
-                    {/*<Box sx={{ flexGrow: 0 }}>*/}
-                    {/*    <Tooltip title="Open settings">*/}
-                    {/*        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>*/}
-                    {/*            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />*/}
-                    {/*        </IconButton>*/}
-                    {/*    </Tooltip>*/}
-                    {/*    <Menu*/}
-                    {/*        sx={{ mt: '45px' }}*/}
-                    {/*        id="menu-appbar"*/}
-                    {/*        anchorEl={anchorElUser}*/}
-                    {/*        anchorOrigin={{*/}
-                    {/*            vertical: 'top',*/}
-                    {/*            horizontal: 'right',*/}
-                    {/*        }}*/}
-                    {/*        keepMounted*/}
-                    {/*        transformOrigin={{*/}
-                    {/*            vertical: 'top',*/}
-                    {/*            horizontal: 'right',*/}
-                    {/*        }}*/}
-                    {/*        open={Boolean(anchorElUser)}*/}
-                    {/*        onClose={handleCloseUserMenu}*/}
-                    {/*    >*/}
-                    {/*        {settings.map((setting) => (*/}
-                    {/*            <MenuItem key={setting} onClick={handleCloseUserMenu}>*/}
-                    {/*                <Typography textAlign="center">{setting}</Typography>*/}
-                    {/*            </MenuItem>*/}
-                    {/*        ))}*/}
-                    {/*    </Menu>*/}
-                    {/*</Box>*/}
                 </Toolbar>
 
-                <Typography variant="h1" sx={{ fontSize: 48, fontWeight: 'bold', mt: 16 }}>
+                <Typography variant="h1" sx={{fontSize: 48, fontWeight: 'bold', mt: 16}}>
                     Odnajdź miejsce na rozwój swojej pasji
                 </Typography>
 
-                <Typography variant="h3" sx={{ fontSize: 24, mt: 4, mb: 4 }}>
+                <Typography variant="h3" sx={{fontSize: 24, mt: 4, mb: 4}}>
                     Szukaj obiektów do zapisu internetowego.
                 </Typography>
 
-                <WhiteBorderTextField label="Read Only" />
+                <Box display="flex" sx={{
+                    boxShadow: 3,
+                    marginBottom: '-30px',
+                    border: '4px solid #1976d2',
+                    backgroundColor: '#1976d2',
+                    mt: 8,
+                    gap: '4px'
+                }}>
+                    <WhiteBorderTextField label="Miejscowość" sx={{flex: 1}}/>
+
+                    <WhiteBorderTextField label="Nazwa obiektu" sx={{flex: 1}}/>
+
+                    <Button variant="contained" sx={{
+                        minHeight: '56px',
+                        fontWeight: 'bold',
+                        boxShadow: 'none',
+                        borderRadius: 0
+                    }}>
+                        Szukaj
+                    </Button>
+                </Box>
             </Container>
         </AppBar>
     );
 }
+
 export default Navigation;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
-import { Button, Container } from "@mui/material";
+import {Button, Container, Typography} from "@mui/material";
 import Navigation from "@/app/components/navigation";
 
 export default function Home() {
-  return (
-    <main>
-        <Navigation />
+    return (
+        <main>
+            <Navigation/>
 
-        <Container maxWidth="lg" sx={{ mt: 5 }}>
-            <Button variant="contained">test</Button>
-        </Container>
-    </main>
-  )
+            <Container maxWidth="lg" sx={{mt: 12}}>
+
+                <Typography variant="h4" sx={{fontWeight: 'bold'}}>
+                    Dostępne obiekty
+                </Typography>
+
+                <Typography variant="h4" sx={{fontWeight: 'bold', mt: 12}}>
+                    Szukaj według rodzaju obiektu
+                </Typography>
+            </Container>
+        </main>
+    )
 }
