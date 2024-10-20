6219faf9465ba75691962def6e400a8946acba6e
lukasion <lukasion@gmail.com>
2024-10-20 23:53:46 +0200

    feat: init vue, quasar, tailwind

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+</script>
+
+<template>
+	<q-header>
+		<q-toolbar class="bg-white shadow-lg q-py-md">
+			<q-avatar>
+				<img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-black.svg">
+			</q-avatar>
+
+			<q-toolbar-title class="text-black">AttendanceHub</q-toolbar-title>
+
+			<q-btn flat round dense icon="whatshot"/>
+		</q-toolbar>
+	</q-header>
+</template>
